3c3609ebdc3908e4b78d94fe32bc08acdc3df1d8
Kudesniza <kudesniza@yandex.ru>
2021-03-13 00:16:04 +0300

    refactor: use another method to solve task

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ module.exports = function towelSort (matrix) {
   if (matrix === undefined || matrix.length === 0 ) {
     return [];
   }
-
-  let array = [];
-  for (let  i=0; i<matrix.length; i++ ) {
-    if ( (i+2)%2 ===0) {
-      for (j = 0; j < matrix[i].length ; j++ ) {
-        array.push(matrix[i][j]);
+  
+    let answer = [];
+    matrix.map(function (array, index) {
+      if (index === 0 || index%2 === 0) {
+          answer = answer.concat(array);
+      } else if (index % 2 != 0) {
+          answer = answer.concat(array.reverse());
       }
-    } else {
-      for (j = matrix[i].length-1;j >= 0 ; j-- ) {
-        array.push(matrix[i][j]);
-      }
-    }
-  }
-  return array;
+    })
+    return answer;
 }
